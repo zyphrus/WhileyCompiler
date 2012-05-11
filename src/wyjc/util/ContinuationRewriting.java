5c709e91ed1a75c41ce6d445f5bcb13ee3a924c0
DavePearce <david.pearce@ecs.vuw.ac.nz>
2012-05-11 19:11:16 +1200

    Moved contiuatuion rewriting into wyjc where it more logically belongs.

--- src/wyjc/util/ContinuationRewriting.java
+++ src/wyjc/util/ContinuationRewriting.java
@@ -24,7 +24,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-package wyjvm.util;
+package wyjc.util;
 
 import static wyjvm.lang.JvmTypes.JAVA_LANG_OBJECT;
 import static wyjvm.lang.JvmTypes.T_BOOL;
