960ef9d45edf922f347d38bf5c9fec50911e47b7
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-03-18 10:35:12 +0400

    WYOCL: added empty builder

---
+++ modules/wyocl/src/wyocl/Wyil2JavaOpenCLBuilder.java
@@ -0,0 +1,13 @@
+package wyocl;
+
+/**
+ * A builder for compiling Wyil files into a combination of Java and OpenCL.
+ * Essentially, certain loops marked for execution on the GPU are compiled into
+ * OpenCL, whilst the remainder is left in Java with appropriate hooks being
+ * installed to interact with the GPU.
+ * 
+ * 
+ */
+public class Wyil2JavaOpenCLBuilder {
+
+}
