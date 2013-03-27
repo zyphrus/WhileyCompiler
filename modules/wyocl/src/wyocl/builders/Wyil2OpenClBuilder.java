37cc224c58c502e677440ee195f78069e0dabf82
DavePearce <david.pearce@ecs.vuw.ac.nz>
2013-03-27 17:34:41 +1300

    WYOCL: working on the builder

--- modules/wyocl/src/wyocl/builders/Wyil2OpenClBuilder.java
+++ modules/wyocl/src/wyocl/builders/Wyil2OpenClBuilder.java
@@ -26,6 +26,8 @@
 package wyocl.builders;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 
 import wybs.lang.Builder;
@@ -33,7 +35,10 @@ import wybs.lang.Logger;
 import wybs.lang.NameSpace;
 import wybs.lang.Path;
 import wybs.util.Pair;
+import wyil.lang.Block;
+import wyil.lang.Code;
 import wyil.lang.WyilFile;
+import wyjvm.lang.ClassFile;
 import wyocl.lang.ClFile;
 
 public class Wyil2OpenClBuilder implements Builder {
@@ -42,7 +47,7 @@ public class Wyil2OpenClBuilder implements Builder {
 	
 	/** 	
 	 * The endLabel is used to determine when we're within a for loop being
-	 * translated in OpenCL. If this is <code>null</code> then we're *not*
+	 * writed in OpenCL. If this is <code>null</code> then we're *not*
 	 * within a for loop. Otherwise, we are.
 	 */
 	private String endLabel;
@@ -67,7 +72,7 @@ public class Wyil2OpenClBuilder implements Builder {
 		long memory = runtime.freeMemory();
 
 		// ========================================================================
-		// Translate files
+		// write files
 		// ========================================================================
 
 		for(Pair<Path.Entry<?>,Path.Entry<?>> p : delta) {
@@ -95,7 +100,126 @@ public class Wyil2OpenClBuilder implements Builder {
 	}	
 	
 	protected ClFile build(WyilFile module) {
-		String r = "This is an Open CL File";
-		return new ClFile(r);
+		StringWriter writer = new StringWriter();
+		PrintWriter pWriter = new PrintWriter(writer);
+		for(WyilFile.MethodDeclaration method : module.methods()) {				
+			build(method,pWriter);			
+		}		
+		return new ClFile(writer.toString());
+	}
+	
+	protected void build(WyilFile.MethodDeclaration method, PrintWriter writer) {
+		for(WyilFile.Case c : method.cases()) {
+			write(c,method,writer);
+		}
+	}
+		
+	protected void write(WyilFile.Case c, WyilFile.MethodDeclaration method,
+			PrintWriter writer) {
+		write(c.body(),c,method,writer);
+	}
+	
+	protected void write(Block b, WyilFile.Case c,
+			WyilFile.MethodDeclaration method, PrintWriter writer) {
+		for(Block.Entry e : b) {
+			write(e,c,method,writer);
+		}
+	}
+			
+	protected void write(Block.Entry entry, WyilFile.Case c,
+			WyilFile.MethodDeclaration method, PrintWriter writer) {
+		try {
+			Code code = entry.code;
+			if(code instanceof Code.BinArithOp) {
+				write((Code.BinArithOp)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.Convert) {
+				write((Code.Convert)code);
+			} else if(code instanceof Code.Const) {
+				write((Code.Const) code, freeSlot, constants, bytecodes);
+			} else if(code instanceof Code.Debug) {
+				write((Code.Debug)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.LoopEnd) {
+				write((Code.LoopEnd)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.AssertOrAssume) {
+				write((Code.AssertOrAssume)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.FieldLoad) {
+				write((Code.FieldLoad)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.ForAll) {
+				freeSlot = write((Code.ForAll)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Goto) {
+				write((Code.Goto)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.If) {				
+				writeIfGoto((Code.If) code, entry, freeSlot, bytecodes);
+			} else if(code instanceof Code.IfIs) {
+				write((Code.IfIs) code, entry, freeSlot, constants, bytecodes);
+			} else if(code instanceof Code.IndirectInvoke) {
+				write((Code.IndirectInvoke)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Invoke) {
+				write((Code.Invoke)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Invert) {
+				write((Code.Invert)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Label) {
+				write((Code.Label)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.BinListOp) {
+				write((Code.BinListOp)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.Lambda) {
+				write((Code.Lambda)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.LengthOf) {
+				write((Code.LengthOf)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.SubList) {
+				write((Code.SubList)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.IndexOf) {
+				write((Code.IndexOf)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Assign) {
+				write((Code.Assign)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Loop) {
+				write((Code.Loop)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Move) {
+				write((Code.Move)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Update) {
+				write((Code.Update)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.NewMap) {
+				write((Code.NewMap)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.NewList) {
+				write((Code.NewList)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.NewRecord) {
+				write((Code.NewRecord)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.NewSet) {
+				write((Code.NewSet)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.NewTuple) {
+				write((Code.NewTuple)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.UnArithOp) {
+				write((Code.UnArithOp)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Dereference) {
+				write((Code.Dereference)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Return) {
+				write((Code.Return)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Nop) {
+				// do nothing
+			} else if(code instanceof Code.BinSetOp) {
+				write((Code.BinSetOp)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.BinStringOp) {
+				write((Code.BinStringOp)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.SubString) {
+				write((Code.SubString)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.Switch) {
+				write((Code.Switch)code,entry,freeSlot,bytecodes);
+			} else if(code instanceof Code.TryCatch) {
+				write((Code.TryCatch)code,entry,freeSlot,handlers,constants,bytecodes);
+			} else if(code instanceof Code.NewObject) {
+				write((Code.NewObject)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.Throw) {
+				write((Code.Throw)code,freeSlot,bytecodes);
+			} else if(code instanceof Code.TupleLoad) {
+				write((Code.TupleLoad)code,freeSlot,bytecodes);
+			} else {
+				internalFailure("unknown wyil code encountered (" + code + ")", filename, entry);
+			}
+
+		} catch (SyntaxError ex) {
+			throw ex;
+		} catch (Exception ex) {		
+			internalFailure(ex.getMessage(), filename, entry, ex);
+		}
 	}
 }
