fb0914c313977c1f6c97ee86c1fabfda768dc59b
David Barnett <david@barnett.net.nz>
2017-07-19 15:55:12 +1200

    Fix Array Length copy invariant
    Now builds up pairs of arrays from the given context then
    finds arrays that have been used in the loop body.
    If any of the pair has been re-assigned it is invalidated.
    If one of the pair is used in the loop body an invariant
    of them remaining equal is added.

--- src/main/java/wyc/builder/invariants/ArrayLengthCopyInvariant.java
+++ src/main/java/wyc/builder/invariants/ArrayLengthCopyInvariant.java
@@ -3,6 +3,8 @@ package wyc.builder.invariants;
 import wyc.builder.LoopInvariantGenerator;
 import wyc.lang.Expr;
 import wyc.lang.Stmt;
+import wycc.util.Pair;
+import wyil.lang.Type;
 
 import java.util.*;
 
@@ -26,11 +28,22 @@ public class ArrayLengthCopyInvariant implements InvariantGenerator {
     public List<Expr> generateInvariant(Stmt.While whileStmt, LoopInvariantGenerator.Context context) {
         List<Expr> invariants = new ArrayList<>();
 
-        Map<Expr.LocalVariable, Expr.LocalVariable> l = findGeneratedArrays(whileStmt, context);
+        Map<Expr.LocalVariable, Expr.LocalVariable> associated =  associatedArrays(context);
+        Map<String, Boolean> validArrays = findArrays(whileStmt, context);
+        Set<Pair<String, String>> added = new HashSet<>();
 
-        for (Map.Entry<Expr.LocalVariable, Expr.LocalVariable> entry : l.entrySet()) {
+        for (Map.Entry<Expr.LocalVariable, Expr.LocalVariable> entry : associated.entrySet()) {
 
-            if (entry.getValue() == null) {
+            // merge mapping with validArrays & associated arrays
+
+
+            if (entry.getValue() == null || !validArrays.getOrDefault(entry.getValue().var, false)) {
+                continue;
+            }
+
+            if (added.contains(new Pair<>(entry.getKey().var, entry.getValue().var)) ||
+                    added.contains(new Pair<>(entry.getValue().var, entry.getKey().var)))
+            {
                 continue;
             }
 
@@ -39,88 +52,179 @@ public class ArrayLengthCopyInvariant implements InvariantGenerator {
             Expr invariant = new Expr.BinOp(Expr.BOp.EQ, lengthOfLocal, lengthOfBase,
                     new LoopInvariantGenerator.GeneratedAttribute("local array is generated with length equal to another array"));
 
+            System.err.println(entry);
+
             invariants.add(invariant);
+            added.add(new Pair<>(entry.getKey().var, entry.getValue().var));
         }
 
         return invariants;
     }
 
-    private Map<Expr.LocalVariable, Expr.LocalVariable> findGeneratedArrays(Stmt.While whileStmt, LoopInvariantGenerator.Context context) {
-        Map<Expr.LocalVariable, Expr.LocalVariable> variants = new HashMap<>();
+    Map<Expr.LocalVariable, Expr.LocalVariable> associatedArrays(LoopInvariantGenerator.Context context) {
 
-        this.findGeneratedArrays(whileStmt.body, variants, true, context);
+        Map<Expr.LocalVariable, Expr.LocalVariable> associated = new HashMap<>();
+
+
+        for (Map.Entry<String, LoopInvariantGenerator.Variable> var : context.getVariables().entrySet()) {
+            if (var.getValue().getType() instanceof Type.Array) {
+                Expr assigned = var.getValue().getAssigned();
+                Expr.LocalVariable left = new Expr.LocalVariable(var.getKey(), var.getValue().getAssigned().attributes());
+                left.type = var.getValue().getType();
+                if (assigned instanceof Expr.LocalVariable) {
+                    Expr.LocalVariable localAssigned = (Expr.LocalVariable) assigned;
+
+                    Expr.LocalVariable right = getArrayGeneratorName(context, localAssigned);
+                    if (right != null) {
+                        associated.put(left, right);
+                        associated.put(right, left);
+                    }
+                } else if (assigned instanceof Expr.ArrayGenerator) {
+                    Expr.ArrayGenerator localGenerated = (Expr.ArrayGenerator) assigned;
+
+                    if (localGenerated.count instanceof Expr.UnOp) {
+                        Expr.UnOp lengthOf = (Expr.UnOp) localGenerated.count;
+
+                        if (lengthOf.mhs instanceof Expr.LocalVariable) {
+                            Expr.LocalVariable countVar = (Expr.LocalVariable) lengthOf.mhs;
+                            if (countVar.type instanceof Type.Array) {
+                                associated.put(left, countVar);
+                                associated.put(countVar, left);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return associated;
+    }
+
+    private Map<String, Boolean> findArrays(Stmt.While whileStmt, LoopInvariantGenerator.Context context) {
+        Map<String, Boolean> variants = new HashMap<>();
+
+        this.findArrays(whileStmt.body, variants, context);
 
         return variants;
     }
 
-    private void findGeneratedArrays(List<Stmt> stmts, Map<Expr.LocalVariable, Expr.LocalVariable> variants, boolean topLevel, LoopInvariantGenerator.Context context) {
+    private void findArrays(List<Stmt> stmts, Map<String, Boolean> variants, LoopInvariantGenerator.Context context) {
         for (Stmt stmt : stmts) {
-            findGeneratedArrays(stmt, variants, topLevel, context);
+            findArrays(stmt, variants, context);
         }
     }
 
-    private void findGeneratedArrays(Stmt stmt, Map<Expr.LocalVariable, Expr.LocalVariable> variants, boolean topLevel, LoopInvariantGenerator.Context context) {
+    private void findArrays(Stmt stmt, Map<String, Boolean> variants, LoopInvariantGenerator.Context context) {
         // only check the guaranteed sections of the loop
         // and ignoring any branches or inner-loops to lessen complexity of identifying variants
         if (stmt instanceof Stmt.IfElse) {
             Stmt.IfElse stmtIfElse = (Stmt.IfElse) stmt;
 
-            this.findGeneratedArrays(stmtIfElse.trueBranch, variants, false, context);
-            this.findGeneratedArrays(stmtIfElse.falseBranch, variants, false, context);
+            this.findArrays(stmtIfElse.trueBranch, variants, context);
+            this.findArrays(stmtIfElse.falseBranch, variants, context);
         } else if (stmt instanceof Stmt.Switch) {
             Stmt.Switch switchStmt = (Stmt.Switch) stmt;
             for (Stmt.Case caseStmt : switchStmt.cases) {
-                this.findGeneratedArrays(caseStmt.stmts, variants, false, context);
+                this.findArrays(caseStmt.stmts, variants, context);
             }
         } else if (stmt instanceof Stmt.DoWhile) {
             Stmt.DoWhile whileStmt = (Stmt.DoWhile) stmt;
             // handle the do-while loop ?
-            this.findGeneratedArrays(whileStmt.body, variants, false, context);
+            this.findArrays(whileStmt.body, variants,  context);
         } else if (stmt instanceof Stmt.NamedBlock) {
             Stmt.NamedBlock namedBlock = (Stmt.NamedBlock) stmt;
-            this.findGeneratedArrays(namedBlock.body, variants, topLevel, context);
+            this.findArrays(namedBlock.body, variants, context);
         } else if (stmt instanceof Stmt.Assign) {
             Stmt.Assign stmtAssign = (Stmt.Assign) stmt;
 
             // would of liked to chain these two lists together
             for (Expr.LVal lval : stmtAssign.lvals) {
-
-                if (lval instanceof Expr.IndexOf) {
-                    Expr.IndexOf indexOf = (Expr.IndexOf) lval;
-
-
-                    if (indexOf.src instanceof Expr.LocalVariable) {
-                        Expr.LocalVariable local = (Expr.LocalVariable) indexOf.src;
-
-                        Optional<Expr.LocalVariable> duplicate = variants.keySet().stream()
-                                .filter(k -> k.var.equals(local.var))
-                                .findFirst();
-
-                        Expr.LocalVariable name = getArrayGeneratorName(context, local);
-
-                        if (name != null &&
-                                (!duplicate.isPresent() || duplicate.get() != null)) {
-                            variants.put(local, name);
-                        } else {
-                            variants.put(local, null);
-                        }
-                    }
-                } else if (lval instanceof Expr.AssignedVariable) {
+                if (lval instanceof Expr.AssignedVariable) {
                     Expr.AssignedVariable assignedVariable = (Expr.AssignedVariable) lval;
-
                     // remove from variants
-                    variants.put(new Expr.LocalVariable(assignedVariable.var), null);
+                    variants.put(assignedVariable.var, false);
+                } else {
 
-                    // removes variants that rely on this value
-                    variants.entrySet()
-                            .stream()
-                            .filter(entry -> entry.getValue() != null && entry.getValue().var.equals(assignedVariable.var))
-                            .map(entry -> entry.getKey())
-                            .forEach(key -> variants.put(key, null));
+                    findArrays(lval, variants);
+
                 }
+            }
+
+            for (Expr rval : stmtAssign.rvals) {
+                findArrays(rval, variants);
             }
         }
     }
+
+    private void findArrays(Expr expr, Map<String, Boolean> arrays) {
+
+        if (expr instanceof Expr.LocalVariable) {
+            Expr.LocalVariable local = (Expr.LocalVariable) expr;
+
+            if (local.type instanceof Type.Array) {
+                arrays.put(local.var, true);
+            }
+        } else  if (expr instanceof Expr.Constant) {
+            // ignore
+        } else if (expr instanceof Expr.IndexOf) {
+            Expr.IndexOf index = (Expr.IndexOf) expr;
+            findArrays(index.src, arrays);
+            findArrays(index.index, arrays);
+        } else if (expr instanceof Expr.BinOp) {
+            Expr.BinOp binOp = (Expr.BinOp) expr;
+
+            findArrays(binOp.lhs, arrays);
+            findArrays(binOp.rhs, arrays);
+        } else if (expr instanceof Expr.UnOp) {
+            Expr.UnOp uOp = (Expr.UnOp) expr;
+
+            findArrays(uOp.mhs, arrays);
+        } else if (expr instanceof Expr.IndirectFunctionOrMethodCall) {
+            Expr.IndirectFunctionOrMethodCall call = (Expr.IndirectFunctionOrMethodCall) expr;
+            for (Expr param : call.arguments) {
+                findArrays(param, arrays);
+            }
+        } else if (expr instanceof Expr.FunctionOrMethodCall) {
+            Expr.FunctionOrMethodCall call = (Expr.FunctionOrMethodCall) expr;
+            for (Expr param : call.arguments) {
+                findArrays(param, arrays);
+            }
+        } else if (expr instanceof Expr.Cast) {
+            Expr.Cast cast = (Expr.Cast) expr;
+
+            findArrays(cast.expr, arrays);
+        } else if (expr instanceof Expr.FieldAccess) {
+            Expr.FieldAccess access = (Expr.FieldAccess) expr;
+
+            findArrays(access.src, arrays);
+        } else if (expr instanceof Expr.ArrayInitialiser) {
+            Expr.ArrayInitialiser initialiser = (Expr.ArrayInitialiser) expr;
+
+            for (Expr e : initialiser.arguments) {
+                findArrays(e, arrays);
+            }
+        } else if (expr instanceof Expr.Record) {
+            Expr.Record record = (Expr.Record) expr;
+
+            for (Expr e : record.fields.values()) {
+                findArrays(e, arrays);
+            }
+        } else if (expr instanceof Expr.ArrayGenerator) {
+            Expr.ArrayGenerator gen = (Expr.ArrayGenerator) expr;
+
+            findArrays(gen.count, arrays);
+            findArrays(gen.element, arrays);
+        } else if (expr instanceof Expr.Dereference) {
+            Expr.Dereference deref = (Expr.Dereference) expr;
+
+            findArrays(deref.src, arrays);
+        } else {
+            throw new IllegalStateException();
+        }
+
+    }
+
 
     /**
      * Get the name of the value
@@ -133,6 +237,9 @@ public class ArrayLengthCopyInvariant implements InvariantGenerator {
         LoopInvariantGenerator.Variable var = context.getValue(local.var);
 
         if (var == null || var.getAssigned() == null) {
+            if (context.hasParam(local.var)) {
+                return local;
+            }
             return null;
         }
 
